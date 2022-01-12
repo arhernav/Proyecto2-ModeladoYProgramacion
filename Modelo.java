2cc6c2a5c34b1bd79701f0ae4e7a45c16ec6e279
Armando Hernández Navarro <arhernav@ciencias.unam.mx>
2022-01-11 20:03:39 -0600

    Iniciode la creacion de clases

---
+++ Modelo.java
@@ -0,0 +1,23 @@
+import java.util.Hashtable;
+/**
+ *Clase para la parte del modelo
+ *Maneja la logistica del programa y el almacenamiento de los usuarios
+ */
+public class Modelo{
+    /* Hashtable que almacena los usuarios de la aplicacion */
+    protected Hashtable<String, Usuario> usuarios;
+
+    /* Estado actual de la aplicacion */
+    protected EstadoModelo estadoActual;
+
+    /* Estado de sesion iniciada de la aplicacion*/
+    protected SesionIniciada sesionIniciada;
+
+    /* Estado de sesion no inicicada de la aplicacion */
+    protected SinSesion sinSesion;
+
+    
+    
+
+    
+}
