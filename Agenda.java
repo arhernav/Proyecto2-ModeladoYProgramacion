b84a13e5fbf1346102f39d16324d51d6ecc9d007
Andrea <107biopanda217>
2022-01-13 00:31:40 -0600

    Se cambió el método de fecha en Agenda.

--- Agenda.java
+++ Agenda.java
@@ -1,45 +1,47 @@
 import java.util.LinkedList;
+import java.time.LocalDate;
 
 /**
  * Clase que registra los gastos que ha realizado el usuario.
  */
 public class Agenda{
-
-
-    ////////////////////////////////////////////////////
-    /////////////Clase interna//////////////////////////
-    ////////////////////////////////////////////////////
+  
+  
+  ////////////////////////////////////////////////////
+  /////////////Clase interna//////////////////////////
+  ////////////////////////////////////////////////////
+  /**
+   * Clase interna que sirve como apoyo para la clase Agenda.
+   * Crea objetos de entrada que serán guardados en la agenda.
+   */
+  public class EntradaAgenda{
+    
     /**
-     * Clase interna que sirve como apoyo para la clase Agenda.
-     * Crea objetos de entrada que serán guardados en la agenda.
+     * Método que imprime la fecha del registro de los gastos del usuario.
      */
-    public class EntradaAgenda{
-	
-	/**
-	 * Método que devuelve con formato la fecha del registro de los gastos del usuario.
-	 * @return String - Fecha del registro de los gastos.
-	 */
-	public String generarFecha(String dia, String mes, String año){
-	    return dia + "/ " + mes + "/ " + año;
-	}
-	
-	/**
-	 * Método que devuelve el registro de los gastos del usuario.
-	 * @return String - Registro de los gastos.
-	 */
-	public String realizarRegistro(String registro){
-	    return registro; 
-	}
-	
+    public void generarFecha(int diaMes, int mes, int año){
+      LocalDate fecha = LocalDate.of(año, mes, diaMes);
+      DateTimeFormatter fechaFormato = DateTimeFormatter.ofPattern("dd - MM - YYYY");
+      System.out.println(fechaFormato.format(fecha));
     }
     
-    //////////////////////////////////////////
-    ///////Fin clase anidada//////////////////
-    //////////////////////////////////////////
+    /**
+     * Método que devuelve el registro de los gastos del usuario.
+     * @return String - Registro de los gastos.
+     */
+    public String realizarRegistro(String registro){
+      return registro; 
+    }
     
-
-    
-
+  }
+  
+  //////////////////////////////////////////
+  ///////Fin clase anidada//////////////////
+  //////////////////////////////////////////
+  
+  
+  
+  
   /** Lista de fechas y registros de los gastos del usuario */
   LinkedList<EntradaAgenda> entradas = new LinkedList<>();
   
@@ -60,19 +62,17 @@ public class Agenda{
   }
   
   /**
-   * Método busca concidencias entre la lista de fechas y la fecha del día de hoy.
-   */
-  /////////////////////////////
-    
-  /**
-   * Método que notifica al usuario sobre los pagos que tiene que realizar
+   * Método busca concidencias entre la lista de fechas y la fecha del día de hoy
+   * para notificar al usuario sobre los pagos que tiene que realizar
    * con un día de anticipación.
    */
   public void pagosARealizar(){
-  ///////////////////  
+    ////////////////////////////////
+    /**
+     * LocalDate -> CompareTo
+     */
   }
+  
+  /********************Calendario*/
+  
 }
-
-/////////////////////////////////////////////////////////////////////////
-
-
